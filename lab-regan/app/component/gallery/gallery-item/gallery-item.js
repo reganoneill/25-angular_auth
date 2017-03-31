8b526c83feb881c6594ba8bead7ccfd86d5ad029
reganoneill <rkrotho@gmail.com>
2017-03-30 20:42:17 -0700

    core functionality

---
+++ lab-regan/app/component/gallery/gallery-item/gallery-item.js
@@ -0,0 +1,21 @@
+'use strict';
+
+require('./_gallery-item.scss');
+
+module.exports = {
+  template: require('./gallery-item.html'),
+  controller: ['$log', 'galleryService', GalleryItemController],
+  controllerAs: 'galleryItemCtrl',
+  bindings: {
+    gallery: '<'
+  }
+};
+
+function GalleryItemController($log, galleryService){
+  $log.debug('GalleryItemController');
+
+  this.showEditGallery = false;
+  this.deleteGallery = function(){
+    galleryService.deleteGallery(this.gallery._id);
+  };
+};
